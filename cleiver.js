d2535ac2c0c7b1610014eaf0794caba9a97c4e63
usuariocuent <117529429+usuariocuent@users.noreply.github.com>
2022-12-03 09:33:46 -0500

    Add files via upload

---
+++ cleiver.js
@@ -0,0 +1,14 @@
+console.log('Inicio de programa');
+
+setTimeout(()=>{
+    console.log('Primer TimeOut');
+}, 3000);
+
+setTimeout(()=>{
+    console,log('Segundo TimeOut')
+},0);
+setTimeout(()=>{
+    console,log('Tercer TimeOut')
+},0);
+
+console.log('Fin del programa');
